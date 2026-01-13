c39e8993e1a13d92137ee9ddb1b1b46cfd0783f7
iheartpink1 <katelynr45@nycstudents.net>
2026-01-13 10:45:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 // YOUR CODE HERE
+
+
+const switch1 = document.querySelector("#switch1");
+const light3 = document.querySelector("#switch3");
+const light8 = document.querySelector("#switch8");
+
+const change38 = () => {
+    const light3 = document.querySelector("#lightbulb3");
+    const light8 = document.querySelector("#lightbulb8");
+}
+
+switch1.addEventListener("click", change38);
+
 console.log("Script Running.");
+
+// const opensesame = () => {
+//     inboxx.classList.add("is-read");
+//     inboxmessage.classList.remove("hidden");
+//     markread.classList.remove("hidden")
+// }
+
+// openbutton.addEventListener("click", opensesame);
